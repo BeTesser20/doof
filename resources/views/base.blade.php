0b08e5f0a295c16424a73d8a779c251262a5990f
BeTesser20 <beatrizptesser@gmail.com>
2024-11-26 08:56:07 -0300

    primeira modificação

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -31,27 +31,27 @@
             </button>
         </div>
         <nav class="text-white text-base font-semibold pt-3">
-            <a href="index.html" class="flex items-center active-nav-link text-white py-4 pl-6 nav-item">
+            <a href="{{ route('index') }}" class="flex items-center active-nav-link text-white py-4 pl-6 nav-item">
                 <i class="fas fa-tachometer-alt mr-3"></i>
-                Dashboard
+                Início
             </a>
-            <a href="blank.html" class="flex items-center text-white opacity-75 hover:opacity-100 py-4 pl-6 nav-item">
+            <a href="{{ route('blank') }}" class="flex items-center text-white opacity-75 hover:opacity-100 py-4 pl-6 nav-item">
                 <i class="fas fa-sticky-note mr-3"></i>
                 Blank Page
             </a>
-            <a href="tables.html" class="flex items-center text-white opacity-75 hover:opacity-100 py-4 pl-6 nav-item">
+            <a href="{{ route('tables') }}" class="flex items-center text-white opacity-75 hover:opacity-100 py-4 pl-6 nav-item">
                 <i class="fas fa-table mr-3"></i>
                 Tables
             </a>
-            <a href="forms.html" class="flex items-center text-white opacity-75 hover:opacity-100 py-4 pl-6 nav-item">
+            <a href="{{ route('forms') }}" class="flex items-center text-white opacity-75 hover:opacity-100 py-4 pl-6 nav-item">
                 <i class="fas fa-align-left mr-3"></i>
                 Forms
             </a>
-            <a href="tabs.html" class="flex items-center text-white opacity-75 hover:opacity-100 py-4 pl-6 nav-item">
+            <a href="{{ route('tabs') }}" class="flex items-center text-white opacity-75 hover:opacity-100 py-4 pl-6 nav-item">
                 <i class="fas fa-tablet-alt mr-3"></i>
                 Tabbed Content
             </a>
-            <a href="calendar.html" class="flex items-center text-white opacity-75 hover:opacity-100 py-4 pl-6 nav-item">
+            <a href="{{ route('calendar') }}" class="flex items-center text-white opacity-75 hover:opacity-100 py-4 pl-6 nav-item">
                 <i class="fas fa-calendar mr-3"></i>
                 Calendar
             </a>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,77 @@
 <?php
 
+use App\Http\Controllers\UsuariosController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('index');
+    return view('inicial');
+})->name('index');
+
+Route::get('/hello', function () {
+    return view('hello');
 });
+
+Route::get('/hello/{nome}', function($nome) {
+    return "Olá {$nome}";
+});
+
+// Route::get('/blank', function() {
+//     return view('blank');
+// });
+
+// Route::get('/calendar', function() {
+//     return view('calendar');
+// });
+
+// Route::get('/forms', function() {
+//     return view('forms');
+// });
+
+// Route::get('/tables', function() {
+//     return view('tables');
+// });
+
+// Route::get('/tabs', function() {
+//     return view('tabs');
+// });
+
+// Route::get('/animais', [AnimaisController::class, 'index'])->name('animais');
+// // quando acassar a rota via get animais ele vai pegar a funcao  controller e executar o index
+
+// Route::get('/animais/cadastrar', [AnimaisController::class, 'cadastrar'])->name('animais.cadastrar');
+
+// Route::post('/animais/cadastrar', [AnimaisController::class, 'gravar'])->name('animais.gravar');
+
+// Route::get('animais/apagar/{animal}', [AnimaisController::class, 'apagar'])->name('animais.apagar');
+
+// Route::delete('animais/apagar/{animal}', [AnimaisController::class, 'deletar']);
+
+// Route::get('/animais/editar/{animal}', [AnimaisController::class, 'editar'])->name('animais.editar');
+
+// Route::put('/animais/editar/{animal}', [AnimaisController::class, 'editarGravar']);
+
+
+// Route::prefix('usuarios')->middleware('auth')->group(function() {
+//     Route::get('/', [UsuariosController::class, 'index'])->name('usuarios');
+
+//     Route::get('/cadastrar', [UsuariosController::class, 'cadastrar'])->name('usuarios.cadastrar');
+
+//     Route::post('/cadastrar', [UsuariosController::class, 'gravar'])->name('usuarios.gravar');
+
+//     Route::get('/apagar/{usuario}', [UsuariosController::class, 'apagar'])->name('usuarios.apagar');
+
+//     Route::delete('/apagar/{usuario}', [UsuariosController::class, 'deletar']);
+
+//     Route::get('/editar/{usuario}', [UsuariosController::class, 'editar'])->name('usuarios.editar');
+
+//     Route::put('/editar/{usuario}', [UsuariosController::class, 'editarGravar']);
+// });
+
+// // quando acassar a rota via get animais ele vai pegar a funcao  controller e executar o index
+
+
+Route::get('login', [UsuariosController::class, 'login'])->name('login');
+
+Route::post('login', [UsuariosController::class, 'login']);
+
+Route::get('logout', [UsuariosController::class, 'logout'])->name('logout');
